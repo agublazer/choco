309944530d79a97d6c077972884ade4f7cf8d30e
Mauro <dany.diaz@ucsp.edu.pe>
2020-06-24 17:53:50 -0500

    Update views.py

--- login/views.py
+++ login/views.py
@@ -6,3 +6,17 @@ def index(request):
         'price1': 100
     }
     return render(request, 'login/index.html', context)
+
+
+def login(request):
+    context = {
+        'price1': 100
+    }
+    return render(request, 'login/login.html', context)
+
+
+def register(request):
+    context = {
+        'price1': 100
+    }
+    return render(request, 'login/register.html', context)
